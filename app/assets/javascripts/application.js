384fdc1a6aa04fa1f69aeced8f699dd59642139f
nadir budhwani <deniro0@yahoo.com>
2016-03-02 12:44:57 -0500

    removed unneeded links, commented out nav in layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require cocoon
 //= require_tree .
