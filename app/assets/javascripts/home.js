7f225eca9faec4e5a831f0b42c2f11f694f49dd1
ALEX SLYE <slyealex@live.com>
2016-03-02 16:08:08 -0500

    remove jQuery from app.js, was causing failing error

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-<script>
+
 $(function() {
-$('a.page-scroll').bind('click', function(event) {
-   var $anchor = $(this);
-   $('html, body').stop().animate({
-       scrollTop: $($anchor.attr('href')).offset().top
-   }, 1500, 'easeInOutExpo');
-   event.preventDefault();
+  $('a.page-scroll').bind('click', function(event) {
+     var $anchor = $(this);
+     $('html, body').stop().animate({
+         scrollTop: $($anchor.attr('href')).offset().top
+     }, 1500, 'easeInOutExpo');
+     event.preventDefault();
+  });
 });
-});
-</script>
