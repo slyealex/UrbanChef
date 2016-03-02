bcb73e4f9d913ffde809774ffdd7fea4a56d9d83
nadir budhwani <deniro0@yahoo.com>
2016-03-02 14:55:36 -0500

    added jquery easings plugin rel link

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+<script>
+$(function() {
+$('a.page-scroll').bind('click', function(event) {
+   var $anchor = $(this);
+   $('html, body').stop().animate({
+       scrollTop: $($anchor.attr('href')).offset().top
+   }, 1500, 'easeInOutExpo');
+   event.preventDefault();
+});
+});
+</script>
