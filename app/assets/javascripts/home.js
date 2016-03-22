aa7e07ba02b3aef3af81513ecbafd9a4b2590f4c
Levi Smith <Levi.Charles.Smith@gmail.com>
2016-03-21 17:28:33 -0700

    JS

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-
+$(document).ready(function() {
   $(function() {
     $('a.page-scroll').bind('click', function(event) {
        var $anchor = $(this);
@@ -50,3 +50,4 @@
     $("#myCarousel").carousel("next");
     });
   });
+})
